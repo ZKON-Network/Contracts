715f3016e0f183af3af9267a237df05c146d2bd0
Nytyr <nytyr@me.com>
2023-10-18 17:08:47 +0200

    feat: 🎸 First version

--- src/accounts/ZkonAccounts.sol
+++ src/accounts/ZkonAccounts.sol
@@ -1,33 +1,88 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.20;
 
 import "./IZkonAccounts.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "../utils/TransferHelper.sol";
 
-contract ZkonAccounts is IZkonAccounts {
-    function registerClient(address clientAddress) external override returns (bool) {
+contract ZkonAccounts is IZkonAccounts, AccessControl {
+
+    // Client => isEnabled
+    mapping(address => bool) public clients;
+
+    // Client => Amount
+    mapping(address => uint256) public amounts;
+
+    // Client => User Id => Encrypted Shards
+    mapping(address => mapping(uint256 => bytes)) private shards;
+
+    address public zkonToken;
+
+    uint256 public signPrice;
+
+    // Client -> Signature Hashes
+    mapping(address => bytes[]) public signs;
+
+    event ProofsSubmited(address indexed client, uint256 amount);
+
+    constructor(address _zkonToken, uint256 _signPrice) {
+        zkonToken = _zkonToken;
+        signPrice = _signPrice;
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function setSignPrice(uint256 price) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        signPrice = price;
+    }
+
+    function registerClient(address clientAddress) external override onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
+        clients[clientAddress] = true;
         return true;
     }
 
     function depositTokens(address client, uint256 amount) external override returns (bool) {
+        TransferHelper.safeTransferFrom(zkonToken, msg.sender, address(this), amount);
+        amounts[client] += amount;
         return true;
     }
 
-    function withdrawTokens(address client, uint256 amount) external override returns (bool) {
+    function withdrawTokens(address client, uint256 amount) external override onlyRole(DEFAULT_ADMIN_ROLE) returns (bool) {
+        require(amount <= amounts[client], "Not enough amount");
+        amounts[client] -= amount;
+        TransferHelper.safeTransfer(zkonToken, msg.sender, amount);
         return true;
     }
 
     function pause(address client) external override {
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender) || msg.sender == client, "Invalid auth");
+        clients[client] = false;
     }
 
     function verifyProofs(address client, bytes[] calldata proofs) external override returns (bool) {
+        for (uint256 i = 0; i < proofs.length; i++) {
+            signs[client].push(proofs[i]);
+        }
+        emit ProofsSubmited(client, proofs.length);
         return true;
     }
 
-    function storeShards(address client, uint256 user, bytes calldata encryptedShards) external override {
-
+    function getProofs(address client, uint256 limit, uint256 offset) public view returns (bytes[] memory) {
+        bytes[] memory proofs = new bytes[](limit);
+        for (uint256 i = 0; i < limit; i++) {
+            proofs[i] = signs[client][offset + i];
+        }
+        return proofs;
     }
 
-    function getShards(address client, uint256 user) external override returns (bytes memory encryptedShards) {
+    function storeShards(address client, uint256 user, bytes calldata encryptedShards) external override {
+        require(amounts[client] > 0, "Not enough balance");
+        require(clients[client], "Client disabled");
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender) || msg.sender == client, "Invalid auth");
+        shards[client][user] = encryptedShards;
+    }
 
+    function getShards(address client, uint256 user) external view override returns (bytes memory encryptedShards) {
+        require(clients[client], "Client disabled");
+        return shards[client][user];
     }
 }
